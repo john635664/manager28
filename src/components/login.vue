da5a9fea5f66c8637235fc6973a3e3448be13937
john635664 <719327325@qq.com>
2019-03-10 15:06:29 +0800

    完成表单验证提示

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,94 @@
 <template>
-    
+    <div class="login">
+        <el-form
+                :model="formData"
+                status-icon
+                :rules="rules"
+                ref="ruleForm2"
+                label-width="top"
+                class="demo-ruleForm"
+        >
+            <h2>用户登录</h2>
+            <el-form-item label="用户名" prop="username">
+                <el-input  v-model.trim="formData.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input type="password" v-model.trim="formData.password" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button class="login-btn" type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        data() {
+            return {
+                formData:{
+                    username:"",
+                    password:""
+                },
+                // 表单的验证规则
+                rules: {
+                    username: [
+                        // required必须 message 提示信息 trigger 触发方式
+                        // min 最小 max 最大
+                        { required: true, message: "用户名不能为空", trigger: "blur" },
+                        { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+                    ],
+                    password: [
+                        // required必须 message 提示信息 trigger 触发方式
+                        // min 最小 max 最大
+                        { required: true, message: "密码不能为空", trigger: "blur" },
+                        { min: 6, max: 18, message: "长度在 6 到 18 个字符", trigger: "change" }
+                    ]
+                }
+            }
+        },
+        methods:{
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        // alert('submit!');
+                        // console.log('success');
+                    } else {
+                        this.$message.error('请正确输入用户名和密码');
+                        return false;
+                    }
+                });
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style>
+    html,
+    body{
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    body>div:first-of-type{
+        height: 100%;
+    }
+    .login{
+        background-color: #324152;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .el-form{
+        width: 580px;
+        height: 440px;
+        box-sizing: border-box;
+        padding: 40px;
+        border-radius: 10px;
+        background-color: white;
+    }
+    .login-btn{
+        width: 100%;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ Vue.config.productionTip = false
 
 import router from './libs/router'
 
+// 导入饿了么ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
 new Vue({
   render: h => h(App),
     router
